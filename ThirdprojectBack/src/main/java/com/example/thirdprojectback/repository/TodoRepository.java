ade6a8f33ed6f8fda950eb45b32cda7eb441e5f4
lamin5 <say8913@gmail.com>
2025-07-04 17:01:53 +0900

    change

--- ThirdprojectBack/src/main/java/com/example/thirdprojectback/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.thirdprojectback.repository;
-
-import com.example.thirdprojectback.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
--- ThirdprojectBack/src/main/java/com/example/thirdprojectback/repository/TodolistRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.thirdprojectback.repository;
-
-import com.example.thirdprojectback.entity.Todolist;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodolistRepository extends JpaRepository<Todolist, Long> {
-}
