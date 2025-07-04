8110ef0eb3c54304f89cae8bb8328ed78c39bcd3
lamin5 <say8913@gmail.com>
2025-07-04 11:16:27 +0900

    back start

---
+++ ThirdprojectBack/src/main/java/com/example/thirdprojectback/ThirdprojectBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.thirdprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdprojectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdprojectBackApplication.class, args);
+	}
+
+}
---
+++ ThirdprojectBack/src/test/java/com/example/thirdprojectback/ThirdprojectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thirdprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdprojectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
