799a070f453863288063519a3c97991a133f5b2b
Ra1nJun <Over4ction@gmail.com>
2025-07-05 15:52:58 +0900

    LH3-19

--- frontend/src/pages/DiseasePage.jsx
+++ frontend/src/pages/DiseasePage.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import styled from 'styled-components';
 import DiseaseSelector from '../components/disease/DiseaseSelector';
 
 const DiseasePage = () => {
-  return(
-    <DiseaseSelector />
+  return (
+    <Wrapper>
+      <DiseaseSelector />
+    </Wrapper>
   );
 };
+
+// styled-components
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background: #fff;
+`;
 
 export default DiseasePage;
