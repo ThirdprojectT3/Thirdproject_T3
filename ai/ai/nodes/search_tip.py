d9f3a1406b4ff932c5c0d6f19e35e27126eba1aa
TP9NS <pshccxx@gmail.com>
2025-07-06 22:33:50 +0900

    generate todo 형식 일치화

--- ai/ai/nodes/search_tip.py
+++ ai/ai/nodes/search_tip.py
@@ -7,7 +7,7 @@ from .time_check import timeit
 def search_tip_from_db(state: GenState) -> GenState:
     found_tips = []
     for todo in state["todo_items"]:
-        query = f"{todo['todoItem']} 운동의 올바른 수행 팁을 알려줘"
+        query = f"{todo['todoItem']} 운동의 올바른 수행 팁을 알려줘 ex) 1. ~~~ 2. ~~~ 3. ~~~"
         tip = query_tip_rag(query)
         found_tips.append({"todoItem": todo["todoItem"], "tip": tip})
         print("tip db 검색")
@@ -17,7 +17,7 @@ def search_tip_from_db(state: GenState) -> GenState:
 def generate_tip_from_gpt(state: GenState) -> GenState:
     new_tips = []
     for todo in state["todo_items"]:
-        prompt = f"'{todo['todoItem']}' 운동을 안전하고 효과적으로 수행하기 위한 팁을 한국어로 3~5줄로 알려줘."
+        prompt = f"'{todo['todoItem']}' 운동을 안전하고 효과적으로 수행하기 위한 팁을 한국어로 3~5줄로 알려줘. ex) 1. ~~~ 2.~~~ 3. ~~~"
         tip = llm.invoke(prompt).content.strip()
         new_tips.append({"todoItem": todo["todoItem"], "tip": tip})
     return {**state, "todo_tips": new_tips}
