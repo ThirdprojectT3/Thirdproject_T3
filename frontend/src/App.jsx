a5fea4a0a49477ccb49e1918502c0d7f5aacae7c
lamin5 <say8913@gmail.com>
2025-07-07 14:31:56 +0900

    private 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+
 import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
@@ -6,18 +7,23 @@ import ProfilePage from './pages/ProfilePage';
 import GraphPage from './pages/GraphPage';
 import DiseasePage from './pages/DiseasePage';
 
+import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/disease" element={<DiseasePage />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/graph" element={<GraphPage />} />
-      </Routes>
-  )
+    <Routes>
+      {/* 공개 라우트 */}
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+
+      {/* 보호된 라우트 */}
+      <Route path="/main" element={<PrivateRoute><MainPage /></PrivateRoute>} />
+      <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+      <Route path="/graph" element={<PrivateRoute><GraphPage /></PrivateRoute>} />
+      <Route path="/disease" element={<PrivateRoute><DiseasePage /></PrivateRoute>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
