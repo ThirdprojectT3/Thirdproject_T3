c17a5786439f2c76b4e774770922e89fdcb9bbee
TP9NS <pshccxx@gmail.com>
2025-07-08 10:59:25 +0900

    spring security cookie로 변경

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,20 +1,11 @@
+// src/api/axios.js
 import axios from "axios";
-import Cookies from "js-cookie";
 
 const instance = axios.create({
   baseURL: "http://localhost:8080/api",
   headers: { "Content-Type": "application/json" },
+  withCredentials: true,
 });
 
-instance.interceptors.request.use(
-  (config) => {
-    const token = Cookies.get('jwtToken');  // 로그인 시 저장한 토큰
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;  // 토큰 자동 추가
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default instance;
